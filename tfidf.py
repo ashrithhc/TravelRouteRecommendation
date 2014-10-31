e50ecd7ccea2bd52b1f57846687e4817a3a1257a
Deeksha S D <deeksha9396@gmail.com>
2014-10-31 12:54:28 +0530

    Modified dbscan for our data set tfidf working

---
+++ tfidf.py
@@ -0,0 +1,54 @@
+import mysql.connector
+from gensim import corpora, models
+import os
+
+
+
+def get_tags(mysql_config):
+	try:
+		conn = mysql.connector.connect(**mysql_config)
+		cursor = conn.cursor()
+		query = "SELECT id,tags FROM photos"
+		cursor.execute(query)
+		rows = cursor.fetchall()
+		f = open('tags.txt','a')
+		for row in rows:
+			text = str(row[0]) + "|" + row[1].encode('utf-8')
+			f.write(text + "\n")
+		f.close()
+	except mysql.connector.Error as err:
+		print query
+		print(err)
+	except Exception as e:
+		print(e)
+
+def calc_tfidf():
+	class MyCorpus(object):
+		def __iter__(self):
+			for line in open('tags.txt'):
+				yield dictionary.doc2bow(line.split('|')[1].lower().split())
+	tags_file = open('tags.txt','r')
+	dictionary = corpora.Dictionary(line.split('|')[1].lower().split() for line in tags_file)
+	print dictionary.token2id
+	print "\n\n"
+	corpus_obj = MyCorpus()
+	corpus = [vec for vec in corpus_obj]
+	tfidf = models.TfidfModel(corpus)
+	corpus_tfidf = tfidf[corpus]
+	for doc in corpus_tfidf:
+		print(doc)
+	# for vec in corpus:
+	# 	print vec
+
+
+if __name__=='__main__':
+
+	mysql_config = {
+		'user': 'root',
+		'password': 'password',
+		'host': '127.0.0.1',
+		'database': 'flickr',
+	}
+
+	# get_tags(mysql_config)
+	calc_tfidf()
